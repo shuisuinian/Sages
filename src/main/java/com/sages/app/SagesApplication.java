704e7061097d6b9184ecba3b22a79407b4741227
wanyifan <2392048412@qq.com>
2019-12-11 16:18:48 +0800

    第一次提交

---
+++ src/main/java/com/sages/app/SagesApplication.java
@@ -0,0 +1,11 @@
+package com.sages.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SagesApplication.class);
+    }
+}
